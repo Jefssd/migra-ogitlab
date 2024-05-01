32d4f7579a5d328a53750c30abdeb961ccaa87ca
jeferson <jno868214@gmail.com>
2024-04-30 21:40:10 -0300

    edições do cadastro do cliente

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <h1> DASHBOARD DANDO GET EM TODOS OS ENDPONTS, COLETAR DADOS DE CLIENTE</h1>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
